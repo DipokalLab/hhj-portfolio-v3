6062a0282e8dfeca66dfe683286729709b8e3475
Dipokal HHJ <hhj@devent.kr>
2024-03-30 17:35:26 +0900

    docs: update posts

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,62 +1,83 @@
-
-
 export default {
-    list: [
-        {
-            title: "GitHub Action + Docker Compose로 배포 자동화",
-            description: "Velog, 깃허브 액션은 깃허브에서 만든 CI/CD이다. CICD에 대해 간략히 설명하자면, Continuous Integration/Continuous Delivery의 약자로 지속적인 통합과 지속적인 배포를 의미한다...",
-            date: "2023-05-05",
-            url: "https://velog.io/@dipokalhhj/GitHub-Action-Docker-Compose%EB%A1%9C-%EB%B0%B0%ED%8F%AC-%EC%9E%90%EB%8F%99%ED%99%94"
-        },
-        {
-            title: "GoCD Pipeline 구축",
-            description: "Velog, GoCD는 이름에서 알 수 있듯이 CD, 지속적인 배포를 관리하는 소프트웨어다. 오픈소스이며 여러 서버를 지원한다. 필자는 맥 미니를 홈서버로 운용중인데, 맥 ARM 환경에서도 동작한다...",
-            date: "2023-07-13",
-            url: "https://velog.io/@dipokalhhj/GoCD-Pipeline-%EA%B5%AC%EC%B6%95"
-        },
-        {
-            title: "게임엔진 없이 게임을 만들었다",
-            description: "Velog, 수시 접수 기간동안 할 일이 없어서 개발했다. 대학 전부를 수능최저 없는 전형으로 지원했고 그에 따라 수능공부를 할 필요가 없으니 남는 시간에는 놀기만 하면 되었다. 남은 시간이 거의 하루 24시간이라 뭐 할거없나 찾아보다 게임을 개발하기로 결정했다. 게임을 만들기 위한 몇 가지 조건을 나열해보면 다음...",
-            date: "2023-09-20",
-            url: "https://velog.io/@dipokalhhj/%EB%82%98%EB%8A%94-%EC%96%B4%EB%96%BB%EA%B2%8C-%EA%B2%8C%EC%9E%84%EC%97%94%EC%A7%84-%EC%97%86%EC%9D%B4-%EA%B2%8C%EC%9E%84%EC%9D%84-%EB%A7%8C%EB%93%A4%EC%97%88%EB%82%98"
-        },
-        {
-            title: "React를 더 이상 쓰지 말아야 합니다",
-            description: "Velog, 글을 시작하기에 앞서 저는 열렬한 React 유저입니다. React와 TypeScript를 사용해서 여러 토이프로젝트를 진행하고 있습니...",
-            date: "2023-10-25",
-            url: "https://velog.io/@dipokalhhj/Abandoned-React"
-        },
-        {
-            title: "내가 실력이 부족하다고 생각하면, 남들을 도우세요",
-            description: "디스콰이엇, 20세기 천재 물리학자 리처드 파인만에게는 독특학 학습법이 있었다. 오늘날 파인만 테크닉이라고도 불리는 학습법인데, 워낙 유명한지라 다들 들어보았으리라 짐작된다. 그래도 간략히 적어보자면 다음과 같다....",
-            date: "2023-08-29",
-            url: "https://dis.qa/4kWms"
-        },
-        {
-            title: "내가 고등학생 개발자라고 밝히지 않는 5가지 이유",
-            description: "디스콰이엇, 1. 고등학생이라는 안전망을 벗어나 현업에 계신 개발자들의 거친 피드백을 받기 위해. 학생이라고 밝힌다면 거친 표현은 많이 사라진다. 그러나 거친 피드백이 있어야만 진정 개발자로 성장할 수 있다고 믿는다. 필자가....",
-            date: "2023-11-14",
-            url: "https://dis.qa/nqNr9H"
-        },
-        {
-            title: "5일간의 음악 플레이어 개발일지",
-            description: "Velog",
-            date: "2022-07-27",
-            url: "https://velog.io/@dipokalhhj/5%EC%9D%BC%EA%B0%84%EC%9D%98-%EC%9D%8C%EC%95%85-%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4-%EA%B0%9C%EB%B0%9C%EC%9D%BC%EC%A7%80"
-        },
-        {
-            title: "JavaScript Boilerplate Code를 리팩토링 하며",
-            description: "Velog",
-            date: "2023-05-25",
-            url: "https://velog.io/@dipokalhhj/JavaScript-Boilerplate-Code%EB%A5%BC-%EB%A6%AC%ED%8C%A9%ED%86%A0%EB%A7%81-%ED%95%98%EB%A9%B0"
-        },
-        {
-            title: "JavaScript 종단간 암호화 (E2E Encryption) 구현 #1",
-            description: "Velog",
-            date: "2023-05-17",
-            url: "https://velog.io/@dipokalhhj/JavaScript-%EC%A2%85%EB%8B%A8%EA%B0%84-%EC%95%94%ED%98%B8%ED%99%94-E2E-Encryption-%EA%B5%AC%ED%98%84-1"
-        },
-
-
-    ]
-}
+  list: [
+    {
+      title: "논리의 엄밀도에 따른 글쓰기 플랫폼 분할",
+      description:
+        "모든 작가로 하여금 자신의 글이 대중에게 읽히기를 원합니다. 그러나 대게 작성한 글의 절대다수는 독자에게 읽히지 않습니다. 제 경우에도 매번 대중성을 목표로 글을 게시하고는 있지만 그러다보니 논리적 비...",
+      date: "2024-03-26",
+      url: "https://medium.com/@hyeongjun/%EB%85%BC%EB%A6%AC%EC%9D%98-%EC%97%84%EB%B0%80%EB%8F%84%EC%97%90-%EB%94%B0%EB%A5%B8-%EA%B8%80%EC%93%B0%EA%B8%B0-%ED%94%8C%EB%9E%AB%ED%8F%BC-%EB%B6%84%ED%95%A0-9e48988bda38",
+    },
+    {
+      title: "GitHub Action + Docker Compose로 배포 자동화",
+      description:
+        "Velog, 깃허브 액션은 깃허브에서 만든 CI/CD이다. CICD에 대해 간략히 설명하자면, Continuous Integration/Continuous Delivery의 약자로 지속적인 통합과 지속적인 배포를 의미한다...",
+      date: "2023-05-05",
+      url: "https://velog.io/@dipokalhhj/GitHub-Action-Docker-Compose%EB%A1%9C-%EB%B0%B0%ED%8F%AC-%EC%9E%90%EB%8F%99%ED%99%94",
+    },
+    {
+      title: "GoCD Pipeline 구축",
+      description:
+        "Velog, GoCD는 이름에서 알 수 있듯이 CD, 지속적인 배포를 관리하는 소프트웨어다. 오픈소스이며 여러 서버를 지원한다. 필자는 맥 미니를 홈서버로 운용중인데, 맥 ARM 환경에서도 동작한다...",
+      date: "2023-07-13",
+      url: "https://velog.io/@dipokalhhj/GoCD-Pipeline-%EA%B5%AC%EC%B6%95",
+    },
+    {
+      title: "게임엔진 없이 게임을 만들었다",
+      description:
+        "Velog, 수시 접수 기간동안 할 일이 없어서 개발했다. 대학 전부를 수능최저 없는 전형으로 지원했고 그에 따라 수능공부를 할 필요가 없으니 남는 시간에는 놀기만 하면 되었다. 남은 시간이 거의 하루 24시간이라 뭐 할거없나 찾아보다 게임을 개발하기로 결정했다. 게임을 만들기 위한 몇 가지 조건을 나열해보면 다음...",
+      date: "2023-09-20",
+      url: "https://velog.io/@dipokalhhj/%EB%82%98%EB%8A%94-%EC%96%B4%EB%96%BB%EA%B2%8C-%EA%B2%8C%EC%9E%84%EC%97%94%EC%A7%84-%EC%97%86%EC%9D%B4-%EA%B2%8C%EC%9E%84%EC%9D%84-%EB%A7%8C%EB%93%A4%EC%97%88%EB%82%98",
+    },
+    {
+      title: "없어지지 않는 직업",
+      description: "링크드인 게시글",
+      date: "2024-02",
+      url: "https://www.linkedin.com/posts/huhhyeongjun_%EC%97%86%EC%96%B4%EC%A7%80%EC%A7%80-%EC%95%8A%EB%8A%94-%EC%A7%81%EC%97%85-%EB%B6%88%EA%B3%BC-%EB%AA%87-%EB%85%84-%EC%A0%84%EB%A7%8C-%ED%95%B4%EB%8F%84-%EB%8B%A8%EC%88%9C-%EB%85%B8%EB%8F%99%EA%B3%BC-%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%97%85%EC%9D%B4-activity-7168892846576496640-DopK?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      title: "비관적인 사람들에게.",
+      description: "링크드인 게시글",
+      date: "2024-02",
+      url: "https://www.linkedin.com/posts/huhhyeongjun_%EB%B9%84%EA%B4%80%EC%A0%81%EC%9D%B8-%EC%82%AC%EB%9E%8C%EB%93%A4%EC%97%90%EA%B2%8C-%EB%B9%84%EA%B4%80%EC%9D%80-%EC%82%B6%EC%97%90-%EC%95%84%EB%AC%B4%EB%9F%B0-%EB%8F%84%EC%9B%80%EC%9D%B4%EC%95%88-%EB%90%A9%EB%8B%88%EB%8B%A4%EB%A6%AC%EC%82%AC-%ED%8E%A0%EB%93%9C%EB%A8%BC-activity-7161626081702137859-y5rt?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      title: "React를 더 이상 쓰지 말아야 합니다",
+      description:
+        "Velog, 글을 시작하기에 앞서 저는 열렬한 React 유저입니다. React와 TypeScript를 사용해서 여러 토이프로젝트를 진행하고 있습니...",
+      date: "2023-10-25",
+      url: "https://velog.io/@dipokalhhj/Abandoned-React",
+    },
+    {
+      title: "내가 실력이 부족하다고 생각하면, 남들을 도우세요",
+      description:
+        "디스콰이엇, 20세기 천재 물리학자 리처드 파인만에게는 독특학 학습법이 있었다. 오늘날 파인만 테크닉이라고도 불리는 학습법인데, 워낙 유명한지라 다들 들어보았으리라 짐작된다. 그래도 간략히 적어보자면 다음과 같다....",
+      date: "2023-08-29",
+      url: "https://dis.qa/4kWms",
+    },
+    {
+      title: "내가 고등학생 개발자라고 밝히지 않는 5가지 이유",
+      description:
+        "디스콰이엇, 1. 고등학생이라는 안전망을 벗어나 현업에 계신 개발자들의 거친 피드백을 받기 위해. 학생이라고 밝힌다면 거친 표현은 많이 사라진다. 그러나 거친 피드백이 있어야만 진정 개발자로 성장할 수 있다고 믿는다. 필자가....",
+      date: "2023-11-14",
+      url: "https://dis.qa/nqNr9H",
+    },
+    {
+      title: "5일간의 음악 플레이어 개발일지",
+      description: "Velog",
+      date: "2022-07-27",
+      url: "https://velog.io/@dipokalhhj/5%EC%9D%BC%EA%B0%84%EC%9D%98-%EC%9D%8C%EC%95%85-%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4-%EA%B0%9C%EB%B0%9C%EC%9D%BC%EC%A7%80",
+    },
+    {
+      title: "JavaScript Boilerplate Code를 리팩토링 하며",
+      description: "Velog",
+      date: "2023-05-25",
+      url: "https://velog.io/@dipokalhhj/JavaScript-Boilerplate-Code%EB%A5%BC-%EB%A6%AC%ED%8C%A9%ED%86%A0%EB%A7%81-%ED%95%98%EB%A9%B0",
+    },
+    {
+      title: "JavaScript 종단간 암호화 (E2E Encryption) 구현 #1",
+      description: "Velog",
+      date: "2023-05-17",
+      url: "https://velog.io/@dipokalhhj/JavaScript-%EC%A2%85%EB%8B%A8%EA%B0%84-%EC%95%94%ED%98%B8%ED%99%94-E2E-Encryption-%EA%B5%AC%ED%98%84-1",
+    },
+  ],
+};
